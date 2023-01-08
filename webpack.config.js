467f74aa35b065fbd1ba791fe4991ad888be501b
meowmin <meowministrator@gmail.com>
2023-01-08 18:27:27 -0500

    gitignored generated .html.go files and added webpack config

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+
+const commonConfig = {
+    mode: "development",
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                use: 'ts-loader',
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.ts', '.js']
+    },
+}
+
+module.exports = [
+    {
+        ...commonConfig,
+        entry: './client/main.ts',
+        output: {
+            path: path.resolve(__dirname, 'www', "js"),
+            filename: 'main.js'
+        }
+    },
+    {
+        ...commonConfig,
+        entry: './client/static/main.ts',
+        output: {
+            path: path.resolve(__dirname, 'www', "js","static"),
+            filename: 'main.js'
+        }
+    }
+]
