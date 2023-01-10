561b70f9f3478c5f61828844cd7ed2afe8e6aafa
meowmin <meowmininstrator@gmail.com>
2023-01-09 20:34:56 -0500

    Abbreviated relative time

--- client/util/time.ts
+++ client/util/time.ts
@@ -31,3 +31,56 @@ export function secondsToTime(s: number): string {
 
     return format("year")
 }
+
+
+export function timeDelta(e: number): number {
+    const t = Math.floor(Date.now() / 1000);
+    return Math.floor((t - e) / 60)
+}
+
+export function relativeTimeAbbreviated(e: number): string {
+    const t = Math.floor(Date.now() / 1000);
+    let s = Math.floor(t - e),
+        o = false;
+    if (s < 1) {
+        if (s > -50) return "now";
+        o = true, s = -s
+    } else if (s < 5) return "now";
+    let i = "";
+    const n = [60, 60, 24, 30, 12],
+        r = ["s", "m", "h", "d", "mo"];
+    for (let e = 0; e < n.length; e++) {
+        if (s < n[e]) {
+            i = `${s}${r[e]}`;
+            break
+        }
+        s = Math.floor(s / n[e])
+    }
+    return "" === i && (i = `${s}y`), o && (i = "in " + i), i
+}
+
+export function relativeSyncedTime(e: number, t: number): string {
+    let s = Math.floor(t - e);
+    if (s < 1) return "0s";
+    let o = "";
+    const i = [60, 60, 24, 30, 12],
+        n = ["s", "m", "h", "d", "mo"];
+    for (let e = 0; e < i.length; e++) {
+        if (s < i[e]) {
+            o = `${s}${n[e]}`;
+            break
+        }
+        s = Math.floor(s / i[e])
+    }
+    return "" === o && (o = `${s}y`), o
+}
+
+// const unhideTimeElement = document.getElementById("unhide-time");
+//
+// export function unhideTimestamps(): void {
+//     unhideTimeElement.innerHTML = ".hidden-time { visibility: visible; font-size: inherit; }"
+// }
+//
+// export function hideTimestamps(): void {
+//     unhideTimeElement.innerHTML = ""
+// }
