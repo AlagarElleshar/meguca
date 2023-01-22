6603ed09888e1c473195f13d0e34cd41c600777c
meowmin <meowmininstrator@gmail.com>
2023-01-22 17:06:57 -0500

    "No." no longer shows on liveposts

--- client/posts/view.ts
+++ client/posts/view.ts
@@ -168,14 +168,12 @@ export default class PostView extends ImageHandler {
         }
 
         const nav = this.el.querySelector("nav"),
-            link = nav.firstElementChild as HTMLAnchorElement,
             quote = nav.lastElementChild as HTMLAnchorElement,
             { id, flag } = this.model
         let url = `#p${id}`
         if (!page.thread && !page.catalog) {
             url = `/all/${id}?last=100` + url
         }
-        link.href = url
         quote.textContent = id.toString()
 
         // Render country flag, if any
