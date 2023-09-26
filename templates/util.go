bee7b3ea1d461aa52136855ca3a24058a1eb6117
meowmin <meowmininstrator@gmail.com>
2023-09-26 05:08:06 -0400

    Moved a function

--- templates/util.go
+++ templates/util.go
@@ -44,21 +44,22 @@ func bold(s string) string {
 	return string(b)
 }
 
-func getTokID(filename string) *string {
+// This function validates tok IDs
+func isValidTokID(digits string) bool {
 	//Max TokID is a week from now
 	//Min TokID is 2016-08-01
 	now := time.Now()
 	maxTokID := now.Add(time.Hour*24*7).Unix() << 32
 	var minTokID int64 = 6313705004335104000
-	//This function validates tok IDs
-	isValidTokID := func(digits string) bool {
-		numDigits := len(digits)
-		if numDigits > 20 || numDigits < 19 {
-			return false
-		}
-		tokID, _ := strconv.ParseInt(digits, 10, 64)
-		return tokID > minTokID && tokID < maxTokID
+	numDigits := len(digits)
+	if numDigits > 20 || numDigits < 19 {
+		return false
 	}
+	tokID, _ := strconv.ParseInt(digits, 10, 64)
+	return tokID > minTokID && tokID < maxTokID
+}
+
+func getTokID(filename string) *string {
 	digits := ""
 	for _, c := range filename {
 		if c >= '0' && c <= '9' {
