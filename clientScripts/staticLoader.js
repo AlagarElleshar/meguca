ccbced750a3d710860de4a30ed1d1972dec9a0c6
meowmin <meowministrator@gmail.com>
2024-04-11 02:11:32 -0400

    bugfix

--- clientScripts/staticLoader.js
+++ clientScripts/staticLoader.js
@@ -3,7 +3,7 @@
 		var head = document.getElementsByTagName('head')[0];
 		var script = document.createElement('script');
 		script.type = 'text/javascript';
-		script.src = '/assets/' + path + '.js';
+		script.src = path;
 		head.appendChild(script);
 		return script;
 	}
