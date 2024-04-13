316412eddca57a71c5464bcf3b969d344642348d
meowmin <meowministrator@gmail.com>
2024-04-13 02:31:05 -0400

    MeguTV stuffs

--- db/images.go
+++ db/images.go
@@ -104,18 +104,11 @@ func GetImageFilename(sha1 string) (exists string, err error) {
 	err = sq.Select("imagename").
 		From("posts").
 		InnerJoin("images on posts.sha1 = images.sha1").
-		Where("sha1 = ?", sha1).
-		OrderBy("posts.id desc").
+		Where("images.sha1 = ?", sha1).
+		OrderBy("posts.id").
 		Limit(1).
 		QueryRow().
 		Scan(&exists)
-	sqlq, _, _ := sq.Select("imagename").
-		From("posts").
-		InnerJoin("images on posts.sha1 = images.sha1").
-		Where("sha1 = ?", sha1).
-		OrderBy("posts.id desc").
-		Limit(1).ToSql()
-	log.Info("SQL: ", sqlq)
 	log.Info("Got filename: ", exists)
 	return
 }
