de2ed5fdbc5f58bb6b62ac05d3740a0d59865370
meowmin <meowministrator@gmail.com>
2024-04-20 17:07:51 -0400

    More protobuf changes

--- pb/generate.go
+++ pb/generate.go
@@ -1,3 +1,3 @@
 package pb
 
-//go:generate protoc --go_out=. --go_opt=paths=source_relative messages.proto
+//go:generate protoc --go_out=. --go_opt=paths=source_relative --plugin=protoc-gen-ts=protoc-gen-ts_proto --ts_proto_out=../client/typings messages.proto
