59a25f6740a25400bc5bbfb5220eaa859fcb32cd
meowmin <meowministrator@gmail.com>
2024-05-02 02:24:17 -0400

    nekoTV may be ready

--- config/config.go
+++ config/config.go
@@ -101,7 +101,10 @@ const defaultFAQ = `Supported upload file types are JPEG, PNG, APNG, WEBM, MP3, 
 #8ball - An 8ball
 #sw24:30 #sw2:24:30 #sw24:30+30 #sw24:30-30 - "Syncwatch" synchronized time counter
 #steal - steal the image of the first post linked; does not work on OPs
-#claude [prompt] - Generates text from a large language model named Claude`
+#claude [prompt] - Generates text from a large language model named Claude
+<hr>NekoWatch commands:
+.play [URL] .remove [URL] .seek [time]
+.pause .unpause .skip .clear`
 
 // Generate /all/ board configs
 func init() {
