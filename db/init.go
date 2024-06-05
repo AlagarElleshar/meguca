2fa6d747458a3f73fb8316e743aa505acf134446
meowmin <meowministrator@gmail.com>
2024-06-05 04:53:25 -0400

    Uses a prepared statement for GetLatestGeneral

--- db/init.go
+++ db/init.go
@@ -141,7 +141,7 @@ func loadDB(connURL, dbSuffix string) (err error) {
 	tasks = append(
 		tasks,
 		func() error {
-			tasks := []func() error{loadConfigs, loadBans, handleSpamScores, prepareUpdatePostsStmt, prepareInsertPostStmt, prepareLinkStatement, prepareInsertImageStmt}
+			tasks := []func() error{loadConfigs, loadBans, handleSpamScores, prepareUpdatePostsStmt, prepareInsertPostStmt, prepareLinkStatement, prepareInsertImageStmt, PrepareGetGeneralStatement}
 			if config.Server.ImagerMode != config.ImagerOnly {
 				tasks = append(tasks, loadBanners, loadLoadingAnimations,
 					loadThreadPostCounts)
