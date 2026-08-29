aab16e888b303596dafb72f9c8fbe37c8d418548
Geckomin <geckomin@geckochen.lol>
2026-08-29 15:14:24 +0200

    Changing requirements again

--- websockets/post_creation.go
+++ websockets/post_creation.go
@@ -189,7 +189,7 @@ func CreatePost(
 		return
 	}
 	count, err := db.CheckIpPostCount(ip)
-	if (hasImage && count <= 1) {
+	if (hasImage && count < 3) {
 		err = common.StatusError{errors.New("You are dissalowed from posting images"), 400}
 		return
 	}
